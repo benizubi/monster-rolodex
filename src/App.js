dff8687a55203a6db2fa2e7dc6feefe113b152f3
benizubi <benizubi@outlook.com>
2021-12-20 23:58:25 +0000

    components

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello my name is Beni Zubi
         </p>
         <a
           className="App-link"
