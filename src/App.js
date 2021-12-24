a047cdcc15c662010cc4b7004a4d26eecada60a2
benizubi <benizubi@outlook.com>
2021-12-24 23:53:49 +0000

    practice

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello my name is Beni Zubi
+          Hello my name is Beni Zubi Kasikis
         </p>
         <a
           className="App-link"
