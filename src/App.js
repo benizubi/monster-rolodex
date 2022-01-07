b47ae706d6716127dfa5c18d6e852ef429ec5aaf
benizubi <benizubi@outlook.com>
2022-01-07 23:46:55 +0000

    class components

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello my name is Beni Zubi
+          Hello my name is Beni
         </p>
         <a
           className="App-link"
