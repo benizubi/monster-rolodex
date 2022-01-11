a256ffa5ee3fe3d77347a5aeebcf73bda27ddc70
benizubi <benizubi@outlook.com>
2022-01-11 23:55:45 +0000

    Dynamic content

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello my name is Kasiki
+          Hello my name is Kasiki Zubis
         </p>
         <a
           className="App-link"
