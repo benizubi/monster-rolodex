bb4866d557bcd7dae8d9aa05ef49b60e07ebf370
benizubi <benizubi@outlook.com>
2022-02-17 19:38:22 +0000

    importing card folder

--- src/App.js
+++ src/App.js
@@ -23,8 +23,7 @@ class App extends React.Component{
   render() {
     return(
       <div className="App">
-        <CardList name ='Beni' >
-          {this.state.monsters.map(monster => (<h1 key={monster.id}> { monster.name }</h1>))}
+        <CardList monsters={this.state.monsters}>
         </CardList>
       
     </div>
