2e5c146fef3a15935a34b145620af452cc26b2ee
benizubi <benizubi@outlook.com>
2022-03-25 23:50:03 +0000

    re rendering

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -3,16 +3,15 @@ import { Component } from 'react';
 class CardList extends Component{
     render() {
         console.log(this.props.monsters);
-        
+        console.log('render from cardlist')
         const { monsters } = this.props;
+        
         return (
             <div>
                 {monsters.map(monster => (
-                    <h1 key={ monster.id}> {monster.name}</h1>
-                 
+                <h1 key={ monster.id}> {monster.name}</h1>  
                   ))}
             </div>
-        
         )
     }
 }
